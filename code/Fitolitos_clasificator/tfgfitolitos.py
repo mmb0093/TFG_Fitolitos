d64c1b57b817edf7ce6b9bb87a11bb5067d05cdf
Marta <mmb0093@alu.ubu.es>
2018-04-22 04:14:14 +0200

    login google

--- code/Fitolitos_clasificator/tfgfitolitos.py
+++ code/Fitolitos_clasificator/tfgfitolitos.py
@@ -1,21 +1,34 @@
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
 from flask import Flask, redirect, url_for
-from flask_dance.contrib.dropbox import make_dropbox_blueprint, dropbox
+from flask_dance.contrib.google import make_google_blueprint, google
 
 app = Flask(__name__)
 app.secret_key = "supersekrit"
-blueprint = make_dropbox_blueprint(
-    app_key="tza9hqk0moy3rh4",
-    app_secret="30jofh5613jmlex",
+blueprint = make_google_blueprint(
+    client_id="927801431905-5kajpv4ihkctidgn1p5l4ctkfmrepnq6.apps.googleusercontent.com",
+    client_secret="KfS0AD-25Q0yp-GfbSww874C",
+    scope=["profile", "email"],
+    offline=True,
+    #redirect_url='/',
 )
 app.register_blueprint(blueprint, url_prefix="/login")
 
+
 @app.route("/")
 def index():
-    if not dropbox.authorized:
-        return redirect(url_for("dropbox.login"))
-    resp = dropbox.post("users/get_current_account")
-    assert resp.ok
-    return "You are {email} on Dropbox".format(email=resp.json()["email"])
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    return "You are {email} on Google".format(email=resp.json()["email"])
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='localhost', port=5001)
+    # app.run(host='0.0.0.0')
+
+
 
