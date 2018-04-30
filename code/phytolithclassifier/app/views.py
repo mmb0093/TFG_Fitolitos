fa06e1d40543cd1cc58647c65a7d7250ec67093d
Marta <mmb0093@alu.ubu.es>
2018-04-30 16:01:36 +0200

    Reorganización del los archivos de flask #25

--- code/phytolithclassifier/app/views.py
+++ code/phytolithclassifier/app/views.py
@@ -1,20 +1,19 @@
 import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from flask_dance.contrib.google import make_google_blueprint, google
+from app import app
 
-app = Flask(__name__)
 app.secret_key = "supersekrit"
 blueprint = make_google_blueprint(
     client_id="927801431905-5kajpv4ihkctidgn1p5l4ctkfmrepnq6.apps.googleusercontent.com",
     client_secret="KfS0AD-25Q0yp-GfbSww874C",
     scope=["profile", "email"],
     offline=True,
-    #redirect_url='/',
 )
 app.register_blueprint(blueprint, url_prefix="/login")
+
 
 
 @app.route("/")
@@ -23,11 +22,9 @@ def index():
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v2/userinfo")
     assert resp.ok, resp.text
-    return "You are {email} on Google".format(email=resp.json()["email"])
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run(host='localhost', port=5001)
-
-
-
+@app.route("/upload-files")
+def upfiles():
+    return render_template('upload')
