11b786b82c0dfcf261b9e3a24400881fd72f75fa
Marta <mmb0093@alu.ubu.es>
2018-05-03 13:38:53 +0200

    feedback subida #29

--- code/phytolithclassifier/app/views.py
+++ code/phytolithclassifier/app/views.py
@@ -3,6 +3,7 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 from flask import  redirect, url_for, render_template, request
 from flask_dance.contrib.google import make_google_blueprint, google
+from werkzeug.utils import secure_filename
 from app import app
 
 app.secret_key = "supersekrit"
@@ -14,9 +15,8 @@ blueprint = make_google_blueprint(
 )
 app.register_blueprint(blueprint, url_prefix="/login")
 
-CARPETA_SUBIDA='/images/uploads'
-EXTENSIONES= set(['zip', 'rar'])
-app.config['CARPETA_SUBIDA'] = CARPETA_SUBIDA
+
+
 
 @app.route("/")
 def index():
@@ -27,8 +27,7 @@ def index():
     return render_template('index.html')
 
 
+
 @app.route("/upload-files", methods=['GET', 'POST'])
 def upfiles():
-    if request.method == 'POST':
-        return 3
     return render_template('upload')
